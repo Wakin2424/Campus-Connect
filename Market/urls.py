5fdee0927e265e7599ab57c0b5eb886b739392ad
CodinSupreme <blairkimanik@gmail.com>
2025-12-06 19:30:25 +0300

    update 1.3.7.1

--- Market/urls.py
+++ Market/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.Home, name='market')
+    path('', views.Home, name='market'),
+    path('<str:category>', views.productLibrary, name='product_library'),
+    path('sell-product', views.uploadProduct, name='upload_product'),
+    path('product/<str:id>/', views.productDetail, name='product_detail'),
 ]
 
--- Market/views.py
+++ Market/views.py
@@ -1,5 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, Http404
 
 # Create your views here.
 def Home(request):
     return render(request, 'market.html')
+
+def productLibrary(request, category):
+    context = {}
+    return render(request, 'product_library.html', context)
+
+def loadProducts(request):
+    pass
+
+def uploadProduct(request):
+    if not request.user.is_authenticated:
+        raise Http404("Invalid Request!")
+    
+    if request.method == "POST":
+        pass
+    
+    else:
+        context = {}
+        return render(request, 'sell_product.html', context)
+
+def productDetail(request, id):
+    context = {}
+    return render(request, 'product_detail.html', context)
